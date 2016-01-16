eaae56d6a5b3148fc347a7acca4195c4a061e4d1
Junqiang Qian <jq514@ic.ac.uk>
2016-01-16 00:40:45 +0000

    Implemented two versions of pairs

--- list.pl
+++ list.pl
@@ -4,3 +4,22 @@ all_members([], _).
 all_members([X|XS], Y) :-
   member(X, Y),
   all_members(XS, Y).
+
+/* Question 2 */
+
+/* Non tail recursive */
+pairs([], []).
+
+pairs([X|XS], Y) :-
+  U is X - 1,
+  V is X + 1,
+  pairs(XS, R),
+  append([(U,V)], R, Y).
+
+/* Tail recursive */
+pairsTR([], []).
+pairsTR([H|T], [(U,V)|Y]) :-
+  U is H - 1,
+  V is H + 1,
+  pairsTR(T, Y).
+
