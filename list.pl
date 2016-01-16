3b49ab487737ff6c37c7675fcb034df037c5baa6
Junqiang Qian <jq514@ic.ac.uk>
2016-01-16 18:43:46 +0000

    Q3 to Q6 implementation

--- list.pl
+++ list.pl
@@ -23,3 +23,64 @@ pairsTR([H|T], [(U,V)|Y]) :-
   V is H + 1,
   pairsTR(T, Y).
 
+/* Question 3 */
+
+arbpairs(X,Y) :- apairs(X, [], Y).
+apairs([], A, A).
+apairs([H|T], A, Y) :-
+  append(A, [(H, H)], UA),
+  apairs(T, UA, Y);
+  L is 2 * H,
+  append(A, [(H, L)], UA),
+  apairs(T, UA, Y).
+
+/* Question 4 */
+
+/* Recursive version */
+
+replace_wrap(X, Y) :- rwrap(X, [], Y).
+rwrap([], A, A).
+rwrap([H|T], A, Y) :-
+  append(A, [wrap(H)], UA),
+  rwrap(T, UA, Y).
+
+/* Findall version */
+
+replace_wrap_FA(X, Y) :- findall(wrap(E), member(E, X), Y).
+
+/* Question 5 */
+
+even_members(X, Y) :- em(X, odd, Y).
+em([], _, []).
+em([_|T], odd, Y) :- em(T, even, Y).
+em([H|T], even, [H|Y]) :- em(T, odd, Y).
+
+/* Question 6 */
+
+odd_even_members(X, Y, Z) :- oem(X, odd, Y, Z).
+oem([], _, [], []).
+oem([H|T], odd, [H|Y], Z) :- oem(T, even, Y, Z).
+oem([H|T], even, Y, [H|Z]) :- oem(T, odd, Y, Z).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
